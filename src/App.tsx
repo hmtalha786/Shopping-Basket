93812b18dde8a5fb7d22f166e8d48ded6e4f6515
Muhammad Talha <talhaofficialwork@gmail.com>
2021-11-29 12:04:05 +0500

    Header component added

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,17 @@ import React from "react";
 import { Product } from "./components/Products";
 import { Basket } from "./components/Basket";
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
+    <>
+    <Header/>
     <div className="App">
       <Product />
       <Basket />
     </div>
+    </>
   );
 }
 
